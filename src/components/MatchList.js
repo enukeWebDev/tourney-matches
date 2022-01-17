a59d5f5b85b85a1cf2da6b456bb1c1ace65e3180
Roderick <enuke4@gmail.com>
2022-01-17 10:36:27 -0600

    add logic to Match.js & MatchList.js compenents

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,10 +3,11 @@ import Match from './Match';
 import matchData from '../data/matchData';
 
 function MatchList(props) {
+  const oneMatch = matchData[0];
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match {...oneMatch} />
     </section>
   );
 };
